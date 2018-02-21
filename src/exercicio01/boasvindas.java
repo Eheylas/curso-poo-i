4bccefc7b6f70785813dea322bf4ad9c7af82315
1714290032 <1714290032@SJB8-D15.academico.iesb.br>
2018-02-21 20:24:47 -0300

    Criação de uma classe

---
+++ src/exercicio01/boasvindas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio01;
+
+/**
+ *
+ * @author 1714290032
+ */
+public class boasvindas {
+    
+    public static void main(String[] args) {
+        System.out.println("Bem Vindos!");
+    }
+    
+}
