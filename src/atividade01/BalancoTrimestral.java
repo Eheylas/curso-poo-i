41a06500ffd7baa176add8a09e4c95235e9a734f
1714290021 <1714290021@SJB8-D19.academico.iesb.br>
2018-02-21 20:57:39 -0300

    Matheus

---
+++ src/atividade01/BalancoTrimestral.java
@@ -0,0 +1,14 @@
+
+package atividade01;
+
+public class BalancoTrimestral {
+    public static void main(String[] args) {
+        double gastosJaneiro = 15040.00;
+        double gastosFevereiro = 23004.70;
+        double gastosMarco = 17010.04;
+        double gastosPrimeiroTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+        
+        System.out.println("Gastos do Primeiro Trimestre: " + gastosPrimeiroTrimestre);
+        
+    }
+}
