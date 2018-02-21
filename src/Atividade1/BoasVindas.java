ec5d874e119a9a02c6d404051321202ebbf9e955
Leandro <Leandro@VULCAN.mshome.net>
2018-02-21 20:31:28 -0300

    Atividade1

---
+++ src/Atividade1/BoasVindas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Atividade1;
+
+/**
+ *
+ * @author Leandro
+ */
+public class BoasVindas {
+    
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo!");
+    }
+    
+}
