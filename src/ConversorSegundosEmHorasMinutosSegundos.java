2ff35857873e0644cfae4f362a7ea5994b8f473f
1714290044 <1714290044@SJB8-D18.academico.iesb.br>
2018-02-21 20:46:43 -0300

    weqwe

---
+++ src/ConversorSegundosEmHorasMinutosSegundos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1714290044
+ */
+public class ConversorSegundosEmHorasMinutosSegundos 
+{
+    public static void main(String[] args) 
+    {
+    double segundos=0f;
+    double horas=segundos/3600;
+    segundos
+    }
+   
+}
