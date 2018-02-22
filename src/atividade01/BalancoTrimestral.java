18103810909c999fa70ea3a6802b536ae3458e95
1714290040 <1714290040@SJB8-D36.academico.iesb.br>
2018-02-21 22:13:39 -0300

    blz! 1.2

--- src/atividade01/BalancoTrimestral.java
+++ src/atividade01/BalancoTrimestral.java
@@ -1,6 +1,8 @@
 
 package atividade01;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author 1714290040
@@ -10,7 +12,8 @@ public class BalancoTrimestral {
       double gastosJaneiro=15040.00, gastosFevereiro= 23004.70, gastosMarco=17010.04;  
       double gastosPrimeiroTrimestre = gastosJaneiro+gastosFevereiro+gastosMarco;
         System.out.println("O gasto trimestral é R$"+gastosPrimeiroTrimestre);
-                
+        JOptionPane.showMessageDialog(null, "O gasto trimestral é R$"+gastosPrimeiroTrimestre);
+        
     }
     
 }
