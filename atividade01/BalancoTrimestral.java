e787d2b2f791a07b28d1abdc07575f1883e792f5
1614290088 <1614290088@SJB8-D22.academico.iesb.br>
2018-02-21 21:15:29 -0300

    INCLUSAO DO GIT. Exercício 2.

---
+++ atividade01/BalancoTrimestral.java
@@ -0,0 +1,10 @@
+
+public class BalancoTrimestral {
+    public static void main (String[] args) {
+        double gastoJaneiro = 15040.00;
+        double gastoFevereiro =    23004.17;
+        double gastoMarco = 17010.04;
+        double gastoPrimeiroTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;
+        System.out.println(gastoPrimeiroTrimestre);
+    }
+}
