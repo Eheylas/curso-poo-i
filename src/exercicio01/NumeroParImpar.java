d6c43a4fc1091c093e7e64a7023393d36b0277b4
1714290032 <1714290032@SJB8-D15.academico.iesb.br>
2018-02-21 21:37:57 -0300

    Ex 04 Impar ou Par

---
+++ src/exercicio01/NumeroParImpar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio01;
+
+/**
+ *
+ * @author 1714290032
+ */
+public class NumeroParImpar {
+    public static void main(String[] args) {
+        double numero = 20;
+        double resto = 5;
+            if (resto == 0){
+                System.out.println("Número é par");
+        
+    } else System.out.println("Número Impar");
+    
+}
+}
