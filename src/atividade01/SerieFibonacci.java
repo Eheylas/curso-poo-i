ba6089ca82e8a204858888aad499af8b839ce19d
1714290021 <1714290021@SJB8-D19.academico.iesb.br>
2018-02-21 21:50:57 -0300

    Matheus1

---
+++ src/atividade01/SerieFibonacci.java
@@ -0,0 +1,16 @@
+
+package atividade01;
+
+public class SerieFibonacci {
+    public static void main(String[] args) {
+        int anterior, numero = 0, proximo = 1;
+        
+        for (int i = 0; i < 10; i++) {
+            anterior = proximo;
+            proximo = proximo + numero;
+            numero = anterior;
+            
+            System.out.println(numero);
+        }
+    }
+}
