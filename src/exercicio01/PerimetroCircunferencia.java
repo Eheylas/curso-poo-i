b17ee473fb52d3bced498f1058f5d471a61e3705
1714290032 <1714290032@SJB8-D15.academico.iesb.br>
2018-02-21 22:01:38 -0300

    Ex 05 - calculadora de perimetro

---
+++ src/exercicio01/PerimetroCircunferencia.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio01;
+
+/**
+ *
+ * @author 1714290032
+ */
+public class PerimetroCircunferencia {
+    public static void main(String[] args) {
+        double pi = 3.1416;
+        double raio = 5;
+        double perimetro = 2*pi*raio;
+        System.out.println("perimetro total: "+perimetro);
+    }
+    
+}
