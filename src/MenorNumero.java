5a533b76c727a43d3e376a19cf450dfd5ebffcbf
1714290010 <1714290010@SJB8-D13.academico.iesb.br>
2018-02-21 21:41:26 -0300

    Concluído exercício 8

---
+++ src/MenorNumero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1714290010
+ */
+import java.util.Scanner;
+
+public class MenorNumero {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Entre com o valor inteiro para X: ");
+        int numeroX = entrada.nextInt();
+        System.out.println("Entre com o valor inteiro para Y: ");
+        int numeroY = entrada.nextInt();
+        if (numeroX > numeroY) {
+            System.out.println("Maior numero: " + numeroX);
+        } else {
+            System.out.println("Maior numero: " + numeroY);
+        }
+    }
+    
+}
