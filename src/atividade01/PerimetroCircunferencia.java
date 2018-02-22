a0803093664852eb91041c782870e698c680bce6
1714290026 <1714290026@SJB8-D17.academico.iesb.br>
2018-02-21 21:12:11 -0300

    Ex.5

---
+++ src/atividade01/PerimetroCircunferencia.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade01;
+
+/**
+ *
+ * @author 1714290026
+ */
+public class PerimetroCircunferencia {
+    public static void main(String[] args) {
+        final double PI = 3.1416;
+        int raio = 40;
+        double perimetro = 2*PI*raio;
+        
+        System.out.println("Perímetro: "+perimetro);
+    }
+}
