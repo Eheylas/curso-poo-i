21acf82c4f2cb4037f4250538192885bb53c445c
Leandro <Leandro@VULCAN.mshome.net>
2018-02-21 21:06:50 -0300

    Atividade1

---
+++ src/Atividade1/BalancoTrimestral.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Atividade1;
+
+/**
+ *
+ * @author Leandro
+ */
+public class BalancoTrimestral {
+    public static void main(String[] args) {
+        double gastosJaneiro = 15040.00;
+        double gastosFevereiro = 23004.70;
+        double gastosMarco = 17010.04;
+        double gastosPrimeiroTrimestre = (gastosJaneiro + gastosFevereiro + gastosMarco);
+        
+        System.out.println("Gastos 1º Trimestre = " + (gastosPrimeiroTrimestre));
+        
+    }
+       
+        
+}
