f0cb4d5cdd6b939c403f67f9f59caa8149db8ac2
Leandro <Leandro@VULCAN.mshome.net>
2018-02-21 22:05:13 -0300

    Atividade1 - Ex.6

--- src/Atividade1/MultiplosDeTres.java
+++ src/Atividade1/MultiplosDeTres.java
@@ -15,7 +15,7 @@ package Atividade1;
 public class MultiplosDeTres {
     public static void main(String[] args) {
         
-        for (int i = 0; i < 101; i++) {
+        for (int i = 0; i < 100; i++) {
             if ((i % 3)==0){
                 System.out.println(i);
             }
