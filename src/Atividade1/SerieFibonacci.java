f24855e95bd52e3188e31d939c2b86cd4adc41f7
Leandro <Leandro@VULCAN.mshome.net>
2018-02-21 22:05:41 -0300

    Atividade1 - Ex.7

---
+++ src/Atividade1/SerieFibonacci.java
@@ -0,0 +1,33 @@
+/*
+7. Escreva um programa que imprima os 10 primeiros números da série de Fibonacci.
+a) Crie uma classe chamada SerieFibonacci com um bloco main.
+b) Dentro do main, declare as variáveis anterior, numero e proximo. 
+c) Inicialize as variáveis numero e proximo com 0 e 1, respectivamente.
+d) Crie um laço que se repita 10 vezes.
+e) Dentro do laço, atualize a variável anterior com a variável proximo.
+f) Dentro do laço, incremente a variável proximo com a variável numero.
+g) Dentro do laço, atualize a variável numero com a variável anterior.
+h) Dentro do laço, imprima a variável numero.
+i) Execute a classe e observe o resultado.
+j) Adicione os arquivos no git remoto.
+ */
+package Atividade1;
+
+/**
+ *
+ * @author Leandro
+ */
+public class SerieFibonacci {
+    public static void main(String[] args) {
+        
+        int anterior;
+        int proximo = 1;
+        int numero = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            
+        }
+           
+     }
+    
+}
