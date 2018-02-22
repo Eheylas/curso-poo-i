97c96ddab938132278cad91540209942ebe7608c
1714290021 <1714290021@SJB8-D19.academico.iesb.br>
2018-02-21 21:10:41 -0300

    Matheus1

--- src/atividade01/NumeroParImpar.java
+++ src/atividade01/NumeroParImpar.java
@@ -4,8 +4,13 @@ package atividade01;
      public static void main(String[] args) {
          int numero = 5;
          int resto = 2;
-         if(resto == 0){
+         int calculo = numero % resto;
+         
+         if(calculo == 0){
             System.out.println("Esse número é par!!");
         }
+         else{
+             System.out.println("Esse número é ímpar!!");
+         }
      }
 }
