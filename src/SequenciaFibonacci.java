05b855e0b47e7ef9070668040c2a534c9337194e
1714290010 <1714290010@SJB8-D13.academico.iesb.br>
2018-02-21 21:34:08 -0300

    Concluído exercício 7

---
+++ src/SequenciaFibonacci.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1714290010
+ */
+public class SequenciaFibonacci {
+    public static void main(String[] args) {
+        int anterior, numero, proximo;
+        numero = 0;
+        proximo = 1;
+        for (int  i = 0; i < 10; i++) {
+            anterior = proximo;
+            proximo += numero;
+            numero = anterior;
+            System.out.println(numero);
+        }
+        
+    }
+    
+}
