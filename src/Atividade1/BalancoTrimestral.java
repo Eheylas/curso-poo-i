341212a64883a587e3e7781cf53819556e605ed3
Leandro <Leandro@VULCAN.mshome.net>
2018-02-21 21:26:31 -0300

    Atividade1 - Ex.2

--- src/Atividade1/BalancoTrimestral.java
+++ src/Atividade1/BalancoTrimestral.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+2. Escreva um programa que calcule e imprima o gasto no primeiro trimestre, sendo que em Janeiro foi gasto R$ 15.040,00, em Fevereiro R$ 23.004,70, e em Março R$ 17.010,04.
+a) Crie uma classe chamada BalancoTrimestral com um bloco main.
+b) Dentro do main, declare as variáveis chamadas gastosJaneiro, gastosFevereiro e gastosMarco, inicializando-as com 15040.00, 23004.70 e 17010.04, respectivamente.
+c) Crie uma variável gastosPrimeiroTrimestre e inicialize-a com a soma das outras três variáveis.
+d) Imprima a variável gastosPrimeiroTrimestre utilizando a classe System.
+e) Execute a classe e observe o resultado.
+f) Adicione os arquivos no git remoto.
  */
 package Atividade1;
 
