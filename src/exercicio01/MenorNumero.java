eb3ef297f3ab39f033b05acf2b25417951c7f91a
1714290032 <1714290032@SJB8-D15.academico.iesb.br>
2018-02-21 22:32:38 -0300

    Ex 08 - Falta construção do código;

---
+++ src/exercicio01/MenorNumero.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio01;
+
+/**
+ *
+ * @author 1714290032
+ */
+import java.util.Scanner;
+public class MenorNumero {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+        entrada
+    }
+
+       
+        
+        
+    }
+
