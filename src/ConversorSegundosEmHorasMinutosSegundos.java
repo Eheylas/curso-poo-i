ec1410bf0a22f6cc25f10e96a15fa745d4d2201f
1714290010 <1714290010@SJB8-D13.academico.iesb.br>
2018-02-21 21:20:32 -0300

    Concluído exercício 4

---
+++ src/ConversorSegundosEmHorasMinutosSegundos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1714290010
+ */
+public class ConversorSegundosEmHorasMinutosSegundos {
+    public static void main(String[] args) {
+        int segundos = 61;
+        int horas = segundos / 3600;
+        segundos = segundos % 3600;
+        int minutos = segundos / 60;
+        segundos = segundos % 60;
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+        
+    }
+}
