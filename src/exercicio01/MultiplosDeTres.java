546a74e8ad8e14d589a9adc5a3a1a12902b02512
1714290032 <1714290032@SJB8-D15.academico.iesb.br>
2018-02-21 22:06:50 -0300

    Ex 06 - Multiplos de 3

---
+++ src/exercicio01/MultiplosDeTres.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio01;
+
+/**
+ *
+ * @author 1714290032
+ */
+public class MultiplosDeTres {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i%3==0){
+                System.out.println("são multiplos de 3: "+i);
+            }
+        }
+    }
+}
