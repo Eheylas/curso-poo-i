17cc11a711b3f6a259660733ab9eacb7985297b4
1714290021 <1714290021@SJB8-D19.academico.iesb.br>
2018-02-21 21:48:36 -0300

    Matheus1

---
+++ src/atividade01/PerimetroCircunferencia.java
@@ -0,0 +1,12 @@
+
+package atividade01;
+
+public class PerimetroCircunferencia {
+    public static void main(String[] args) {
+        double pi = 3.1416;
+        double raio = 2;
+        double perimetro = 2*pi*raio;
+        
+        System.out.println("Perímetro: "+perimetro);
+    }
+}
