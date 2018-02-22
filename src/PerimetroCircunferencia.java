3186fc543b674d82eaf10d904674e782939c130c
1714290022 <1714290022@SJB8-D11.academico.iesb.br>
2018-02-21 22:05:33 -0300

    1-5

---
+++ src/PerimetroCircunferencia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1714290022
+ */
+public class PerimetroCircunferencia {
+    public static void main(String[] args) {
+        float r = 1;
+        double p = 2*(3.1416)*r;
+        
+        System.out.println("O perímetro é igual a:"+p);
+        
+        
+                
+    }
+}
