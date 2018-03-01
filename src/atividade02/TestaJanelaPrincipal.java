b6c38f917a7316fa4e30937a1119026b049849f6
1524290010 <1524290010@SJB8-D04.academico.iesb.br>
2018-02-28 21:45:35 -0300

    primeira janela feita

---
+++ src/atividade02/TestaJanelaPrincipal.java
@@ -0,0 +1,22 @@
+
+
+package atividade02;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 1524290010
+ */
+public class TestaJanelaPrincipal {
+public static void main (String[] args){
+    JFrame janela = new JFrame();
+    janela.setTitle("Janela Principal");
+    janela.setSize(640, 480);
+    janela.setVisible(true);
+    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+    janela.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    
+}
+    
+}
