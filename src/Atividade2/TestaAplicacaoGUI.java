34bd58ba9d8c1d9e9bcda76b5dc31c94c0c21b2c
Leandro <Leandro@VULCAN.mshome.net>
2018-02-28 22:39:12 -0300

    Atividade2 - Ex.4

---
+++ src/Atividade2/TestaAplicacaoGUI.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Atividade2;
+
+/**
+ *
+ * @author Leandro
+ */
+public class TestaAplicacaoGUI {
+    public static void main(String[] args) {
+        JanelaPrincipal janelaPrincipal = new JanelaPrincipal();
+        janelaPrincipal.exibir();
+    
+    }  
+}
