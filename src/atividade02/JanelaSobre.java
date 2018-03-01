945555cbff88f81ad404b1a94a185e75729cef0d
1714290026 <1714290026@SJB8-D17.academico.iesb.br>
2018-02-28 22:15:37 -0300

    Ex.2-4

---
+++ src/atividade02/JanelaSobre.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade02;
+
+import java.awt.Frame;
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author 1714290026
+ */
+public class JanelaSobre extends JDialog {
+
+    public JanelaSobre(Frame frame, boolean bln) {
+        super(frame, bln);
+    }
+    
+    public void exibir() {
+        setSize(320,240);
+        setTitle("Sobre...");
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
---
+++ src/atividade02/TestaAplicacaoGUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade02;
+
+/**
+ *
+ * @author 1714290026
+ */
+public class TestaAplicacaoGUI {
+    
+    public static void main(String[] args) {
+        JanelaPrincipal janelaPrincipal = new JanelaPrincipal();
+        
+        janelaPrincipal.exibir();
+    }
+    
+}
