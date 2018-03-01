dea87ebf88d63527e1e02483fc42a94c62b8043f
1714290024 <1714290024@SJB8-D24.academico.iesb.br>
2018-02-28 22:44:16 -0300

    Criação de janelas, Aula 2.

---
+++ src/atividade02/JanelaPrincipal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade02;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 1714290024
+ */
+public class JanelaPrincipal extends JFrame{
+    private JanelaSobre janelaSobre;
+    
+}
