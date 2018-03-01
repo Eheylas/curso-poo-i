801ff376cd22140f73104fd3c6a1e782bc79c00d
1714290007 <1714290007@SJB8-D05.academico.iesb.br>
2018-02-28 22:42:43 -0300

    Exercicio 2 a fazer

---
+++ src/atividade02/TestaCaixaDialogo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade02;
+
+import javax.swing.JDialog;
+
+/**
+ *
+ * @author 1714290007
+ */
+public class TestaCaixaDialogo {
+    public static void main(String[] args) {
+        JDialog caixaDialogo = new JDialog();
+        caixaDialogo.setTitle("Caixa de Diálogo");
+        caixaDialogo.setSize(320, 240);
+        caixaDialogo.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        caixaDialogo.setVisible(true);
+          
+        
+    }
+    
+}
