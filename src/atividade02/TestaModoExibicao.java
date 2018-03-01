a5238e404b3234aa383d7c52697a89ff990b0bb2
1714290026 <1714290026@SJB8-D17.academico.iesb.br>
2018-02-28 21:51:56 -0300

    Ex.2-3

---
+++ src/atividade02/TestaModoExibicao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade02;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 1714290026
+ */
+public class TestaModoExibicao {
+    public static void main(String[] args) {
+        JFrame janelaPrincipal = new JFrame();
+        janelaPrincipal.setSize(640,480);
+        janelaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janelaPrincipal.setVisible(true);
+        
+        JDialog caixaDialogo = new JDialog(janelaPrincipal, true);
+        caixaDialogo.setSize(320,240);
+        caixaDialogo.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        caixaDialogo.setVisible(true);
+    }
+}
