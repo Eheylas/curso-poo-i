22c41d81f7c85e7e1ae989e4e627ac0dc49a20c0
FAS <FAS@LAPTOP-CVUSR2JS>
2018-03-07 22:25:41 -0300

    ALERTAS EXERCICIO_02

---
+++ src/exercicio03/TestaMensagemBoasVindas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio03;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author FAS
+ */
+public class TestaMensagemBoasVindas {
+
+    public static void main(String[] args) {
+
+        int opcao = JOptionPane.showConfirmDialog(null, "Deseja iniciar o programa?");
+        String nome = "";
+        if (opcao == JOptionPane.YES_OPTION) {
+            nome = JOptionPane.showInputDialog(null, "insira seu nome: ");
+        
+        if (nome.equals(
+                "")) {
+            JOptionPane.showMessageDialog(null, "Erro, nome não informado", "Erro", JOptionPane.ERROR_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Bem-vindo, " + nome, "Bem-vindo!", JOptionPane.INFORMATION_MESSAGE);
+        }
+       
+        } else {
+            JOptionPane.showMessageDialog(null, "O programa foi encerrado", "Aviso", JOptionPane.WARNING_MESSAGE);
+    }
+}
+}
