78bd5c0cb7f5bcae436dcabf4ac06aaab3486fa9
FAS <FAS@LAPTOP-CVUSR2JS>
2018-03-07 21:19:19 -0300

    Atualização2

--- src/exercicio01/MenorNumero.java
+++ src/exercicio01/MenorNumero.java
@@ -10,14 +10,28 @@ package exercicio01;
  * @author 1714290032
  */
 import java.util.Scanner;
+
 public class MenorNumero {
+
     public static void main(String[] args) {
-       Scanner entrada = new Scanner(System.in);
-        entrada
-    }
+        int num1;
+        int num2;
 
-       
-        
-        
-    }
+        Scanner entrada = new Scanner(System.in);
+        {
+            System.out.printf("Informe um número para comparação: ");
+            num1 = entrada.nextInt();
 
+            System.out.printf("Informe um número para a comparação: ");
+            num2 = entrada.nextInt();
+        }
+
+        if (num1 > num2) {
+            System.out.println("O número " + num1 + " é o maior");
+        } else if (num1 == num2) {
+            System.out.println("São iguais!!!!");
+        } else {
+            System.out.println("O número " + num2 + " é o maior");
+        }
+    }
+}
