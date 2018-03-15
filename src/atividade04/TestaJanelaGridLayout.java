14980e10d612e99f0269883e691941e06c6d20a6
1714290044 <1714290044@SJB8-D18.academico.iesb.br>
2018-03-14 21:27:08 -0300

    ueueue

---
+++ src/atividade04/TestaJanelaGridLayout.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade04;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TestaJanelaGridLayout 
+{
+    public static void main(String[] args) 
+    {
+        JPanel painel1 = new JPanel();
+        painel1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        JPanel painel2 = new JPanel();
+         painel2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        JPanel painel3 = new JPanel();
+         painel3.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        JPanel painel4 = new JPanel();
+         painel4.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        JPanel painel5 = new JPanel();
+         painel5.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+         
+         JFrame janela = new JFrame();
+        
+        GridLayout leiaute= new GridLayout(3,2);
+        janela.setLayout(leiaute);
+        
+        
+        janela.add(painel1);
+        janela.add(painel2);
+        janela.add(painel3);
+        janela.add(painel4);
+        janela.add(painel5);
+
+        janela.setSize(640,480);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+    }
+}
