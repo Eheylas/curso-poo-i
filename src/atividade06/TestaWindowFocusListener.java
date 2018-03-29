98281a368a3747e51f5f6734466b3bf5555f09f3
1714290024 <1714290024@SJB8-D24.academico.iesb.br>
2018-03-28 21:43:02 -0300

    Ouvintes de Janela, Aula 10/11

---
+++ src/atividade06/TestaWindowFocusListener.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade06;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 1714290024
+ */
+public class TestaWindowFocusListener {
+    public static void main(String[] args) {
+        OuvinteWindowFocusListener ouvinte = new OuvinteWindowFocusListener();
+        JFrame janela = new JFrame();
+        
+        janela.addWindowFocusListener(ouvinte);
+        
+        janela.setSize(300, 200);
+        
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        janela.setVisible(true);
+    }
+}
