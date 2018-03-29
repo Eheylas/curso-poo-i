dbc852380a5f8adce64698eb7d5b9806733556a2
1714290024 <1714290024@SJB8-D24.academico.iesb.br>
2018-03-28 21:42:57 -0300

    Ouvintes de Janela, Aula 10/11

---
+++ src/atividade06/OuvinteWindowFocusListener.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade06;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+/**
+ *
+ * @author 1714290024
+ */
+public class OuvinteWindowFocusListener implements WindowFocusListener {
+
+    @Override
+    public void windowGainedFocus(WindowEvent we) {
+        System.out.println("Janela ganhou o foco");
+    }
+
+    @Override
+    public void windowLostFocus(WindowEvent we) {
+        System.out.println("Janela perdeu o foco");
+    }
+    
+}
