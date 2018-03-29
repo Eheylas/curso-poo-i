c442772d6d1ce104e2349132336d836adbb0f492
1714290024 <1714290024@SJB8-D24.academico.iesb.br>
2018-03-28 21:52:24 -0300

    Ouvintes de Janela, Aula 10/11

---
+++ src/atividade06/OuvinteWindowStateListener.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade06;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+
+/**
+ *
+ * @author 1714290024
+ */
+public class OuvinteWindowStateListener implements WindowStateListener {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void windowStateChanged(WindowEvent we) {
+        System.out.println("A janela passou de \" + e.getOldState() + \" para \" + e.getNewState().");
+    }
+}
