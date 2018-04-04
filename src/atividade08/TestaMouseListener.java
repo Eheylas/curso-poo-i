782e2a2e2c4950878b70732e617d0f8630f39f4b
1714290001 <1714290001@SJB8-D12.academico.iesb.br>
2018-04-04 20:20:56 -0300

    Eventos de Mouse, Aula12

---
+++ src/atividade08/TestaMouseListener.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade08;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 1714290001
+ */
+public class TestaMouseListener {
+    public static void main(String[] args) {
+        OuvinteMouseListener ouvinte  = new OuvinteMouseListener();
+        JFrame janela = new JFrame();
+        
+        janela.addMouseListener (ouvinte);
+        janela.setSize(300, 200);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+        
+        
+    }
+}
