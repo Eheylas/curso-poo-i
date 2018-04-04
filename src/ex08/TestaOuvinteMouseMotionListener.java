7c2fa856595403220be803d0dd683c8c895d3c1b
FAS <FAS@LAPTOP-CVUSR2JS>
2018-04-04 20:31:53 -0300

    Ex08 - ex2

---
+++ src/ex08/TestaOuvinteMouseMotionListener.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex08;
+
+import java.awt.event.MouseMotionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author FAS
+ */
+public class TestaOuvinteMouseMotionListener {
+
+    public static void main(String[] args) {
+
+        OuvinteMouseMotionListener ouvinte = new OuvinteMouseMotionListener(); 
+
+        JFrame janela = new JFrame();
+
+        janela.addMouseMotionListener(ouvinte);
+
+        janela.setSize(
+                300, 200);
+        janela.setDefaultCloseOperation(
+                JFrame.EXIT_ON_CLOSE
+        );
+
+        janela.setVisible(
+                true);
+    }
+}
