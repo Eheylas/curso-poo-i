bbc3b80b6e06056cdba840aa98a0a779259a1948
1714290011 <1714290011@SJB8-D16.academico.iesb.br>
2018-04-25 22:11:48 -0300

    TestaFormularioBotao

---
+++ src/atividade10/TestaFormularioBotao.java
@@ -0,0 +1,11 @@
+
+package atividade10;
+
+
+public class TestaFormularioBotao {
+    public static void main(String[] args) {
+        FormularioBotao formulario = new FormularioBotao();
+        formulario.setVisible(true);
+        
+    }
+}
