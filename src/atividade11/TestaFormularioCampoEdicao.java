abf6df60178472e551ef37869de2c1cfad894061
1714290024 <1714290024@SJB8-D23.academico.iesb.br>
2018-05-02 22:35:28 -0300

    Adicionando Campos de Texto e Seleção, Aulas 16 e 17.

---
+++ src/atividade11/TestaFormularioCampoEdicao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade11;
+
+/**
+ *
+ * @author 1714290024
+ */
+public class TestaFormularioCampoEdicao {
+    public static void main(String[] args) {
+        FormularioCampoEdicao formulario = new FormularioCampoEdicao();
+        formulario.setVisible(true);
+    }
+}
