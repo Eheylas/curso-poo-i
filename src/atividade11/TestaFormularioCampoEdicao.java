5469ca8c4b4f15a54006e9f0ded83c888893c8d4
1714290011 <1714290011@SJB8-D16.academico.iesb.br>
2018-05-02 22:10:03 -0300

    TestaFormularioCampoEdicao

---
+++ src/atividade11/TestaFormularioCampoEdicao.java
@@ -0,0 +1,10 @@
+
+package atividade11;
+
+
+public class TestaFormularioCampoEdicao {
+    public static void main(String[] args) {
+        FormularioCampoEdicao formulario = new FormularioCampoEdicao();
+        formulario.setVisible(true);
+    }
+}
